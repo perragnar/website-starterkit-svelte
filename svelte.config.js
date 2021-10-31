c4842dc4714b8c433e7d80179554d2a0849881fb
Per Ragnar Edin <perragnar@gmail.com>
2021-10-31 18:59:12 +0100

    Add Svelte static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,16 @@
 import sveltePreprocess from 'svelte-preprocess';
 import path from 'path';
+import adapter from '@sveltejs/adapter-static';
 
 const production = !process.env.ROLLUP_WATCH;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: sveltePreprocess({
-		sourceMap: !production,
+		sourceMap: !production
 	}),
 	kit: {
+		adapter: adapter(),
 		target: '#svelte',
 		vite: {
 			resolve: {
